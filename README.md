06a17c630e5046a882dbb89daa8ec6c476d8b4c2
teejay_7 <twasam.john@gmail.com>
2022-10-27 17:46:57 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# teleBot
+# teleBot
+# teleBot
---
+++ index.js
@@ -0,0 +1,46 @@
+import pkg from "@google-cloud/functions-framework"
+const functions =pkg
+import fetch from "node-fetch"
+
+const API ="https://api.telegram.org/bot<replacewithyourtoken>"
+const URL = "https://cloudfunctionurl"
+
+
+const init =()=>{
+    fetch(`${API}/setWebhook?url=${URL}`)
+    .then(resp =>resp.json())
+    .then(data =>{
+        console.log("WEBHHOOK SET")
+       return console.log(data)
+    })
+}
+
+const replyMsg =(id, text)=>{
+    const body = {
+        chat_id: id,
+        text
+    }
+    fetch(`${API}/sendMessage`,{
+        method:'post',
+        body:JSON.stringify(body),
+        headers: {'Content-Type': 'application/json'}
+    })
+    .then(resp =>resp.json())
+    .then(data =>{
+        console.log("reply sent")
+       return console.log(data)
+    })
+}
+
+functions.http('teleBot', (request, response)=>{
+    // set your webhook url
+    init()
+
+    const {chat, text}=request.body.message
+    // Reply with the same text sent 
+    replyMsg(chat.id, text)
+    //Handling upload of audio to telegram and processing it with text to speech API
+    // TODO
+    
+    response.status(200).send("Hello GCP\n")
+})
